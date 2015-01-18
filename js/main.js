0cc1b412db52279ac2492f9dc7c325ec79af035b
Sleepwalker <mail@asleepwalker.ru>
2015-01-18 02:50:17 +0200

    Added calendar heatmap

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
 $(function() {
+
+	moment.locale('ru');
+
+	var cal = new CalHeatMap();
+	cal.init({
+		itemSelector: '#matrix',
+		domain: 'month',
+		subDomain: 'day',
+		data: 'matrix.php',
+		start: new Date((new Date()).getFullYear(), 0, 1),
+		cellSize: 10,
+		domainGutter: 5,
+		legend: [20, 40, 60, 80],
+		subDomainDateFormat: function(date) {
+			return moment(date).format('LL');
+		},
+		domainLabelFormat: function(date) {
+			return moment(date).format('MMMM');
+		},
+		legendTitleFormat: {
+			lower: 'КПД ниже {min} %',
+			inner: 'КПД {down}-{up} %',
+			upper: 'КПД выше {max} %'
+		}
+	});
+
 	$('.time-select .time').timepicker({
 	    'showDuration': true,
 	    'timeFormat': 'H:i',
@@ -9,4 +35,5 @@ $(function() {
 		}
 	});
 	$('.time-select').datepair();
+
 });
