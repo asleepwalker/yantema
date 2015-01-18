48a8a9f13edf024c8bd5769eab8e4a1dcf9fae52
Sleepwalker <mail@asleepwalker.ru>
2015-01-18 03:15:39 +0200

    Redesigned matrix

--- js/main.js
+++ js/main.js
@@ -5,18 +5,21 @@ $(function() {
 	var cal = new CalHeatMap();
 	cal.init({
 		itemSelector: '#matrix',
-		domain: 'month',
+		domain: 'week',
 		subDomain: 'day',
 		data: 'matrix.php',
-		start: new Date((new Date()).getFullYear(), 0, 1),
-		cellSize: 10,
-		domainGutter: 5,
+		start: (new Date()).setFullYear((new Date()).getFullYear()-1),
+		cellSize: 12,
+		domainGutter: 0,
+		highlight: 'now',
+		range: 53,
 		legend: [20, 40, 60, 80],
 		subDomainDateFormat: function(date) {
 			return moment(date).format('LL');
 		},
 		domainLabelFormat: function(date) {
-			return moment(date).format('MMMM');
+			return '';
+			//return moment(date).format('MMMM');
 		},
 		legendTitleFormat: {
 			lower: 'КПД ниже {min} %',
