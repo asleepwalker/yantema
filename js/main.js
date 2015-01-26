c5d444924223491273d1131e27c525c0c08657d1
Sleepwalker <mail@asleepwalker.ru>
2015-01-26 14:54:18 +0200

    Removed trash from code and configs

--- js/main.js
+++ js/main.js
@@ -1,31 +1,6 @@
 $(function() {
 
-	/*moment.locale('ru');
-
-	var cal = new CalHeatMap();
-	cal.init({
-		itemSelector: '#reports .matrix',
-		domain: 'week',
-		subDomain: 'day',
-		//data: 'matrix.php',
-		start: (new Date()).setFullYear((new Date()).getFullYear()-1),
-		cellSize: 13,
-		domainGutter: 0,
-		highlight: 'now',
-		range: 53,
-		legend: [20, 40, 60, 80],
-		subDomainDateFormat: function(date) {
-			return moment(date).format('LL');
-		},
-		domainLabelFormat: function(date) {
-			return '';
-		},
-		legendTitleFormat: {
-			lower: 'КПД ниже {min} %',
-			inner: 'КПД {down}-{up} %',
-			upper: 'КПД выше {max} %'
-		}
-	});*/
+	moment.locale('ru');
 
 	$('.time-select .time').timepicker({
 	    'showDuration': true,
