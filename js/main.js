5b4012916cb4e719e09a2c59c23985a025f37ab5
Sleepwalker <mail@asleepwalker.ru>
2015-01-26 14:48:20 +0200

    Finished with tasks page design

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function() {
 
-	moment.locale('ru');
+	/*moment.locale('ru');
 
 	var cal = new CalHeatMap();
 	cal.init({
@@ -25,7 +25,7 @@ $(function() {
 			inner: 'КПД {down}-{up} %',
 			upper: 'КПД выше {max} %'
 		}
-	});
+	});*/
 
 	$('.time-select .time').timepicker({
 	    'showDuration': true,
@@ -37,5 +37,30 @@ $(function() {
 		}
 	});
 	$('.time-select').datepair();
+
+	function reportsAutosize(scrollBottom) {
+		var windowHeight = $(window).height(),
+			reportsOffsetTop, reportsHeight, formHeight, reportsMaxHeight;
+
+		$('.reports').removeClass('has-scroll').css('max-height', '');
+		if (windowHeight > 600) {
+			reportsOffsetTop = $('.reports').offset().top,
+			reportsHeight = $('.reports').height(),
+			formHeight = $('.add-report').outerHeight(true),
+			reportsMaxHeight = windowHeight - (reportsOffsetTop + formHeight)
+			$('.reports').css('max-height', reportsMaxHeight + 'px');
+
+			if (reportsMaxHeight < reportsHeight) {
+				$('.reports').addClass('has-scroll');
+
+				if (scrollBottom === true) {
+					$('.reports').animate({ scrollTop: (reportsHeight - reportsMaxHeight) }, 300);
+				}
+			}
+		}
+	}
+
+	reportsAutosize(true);
+	$(window).resize(reportsAutosize);
 
 });
