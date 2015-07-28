dca8b2f2f7798da583c2fc291e5790eae74872a9
Sleepwalker <mail@asleepwalker.ru>
2015-07-28 23:55:09 +0300

    Added Gruntfile, js linting task and assets preparing task

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		clean: {
+			assets: 'public/assets/**/*'
+		},
+		copy: {
+			assets: {
+				expand: true,
+				cwd: 'bower_components/',
+				src: '**/*',
+				dest: 'public/assets/'
+			},
+		},
+		jshint: {
+			files: ['Gruntfile.js', '{bin,public,tests}/**/*.js'],
+			options: {
+				jshintrc: 'build/.jshintrc'
+			}
+		},
+		jscs: {
+				src: ['Gruntfile.js', '{bin,public,tests}/**/*.js'],
+				options: {
+					config: 'build/.jscsrc'
+				}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-jscs');
+
+	grunt.registerTask('lint', ['jshint', 'jscs']);
+	grunt.registerTask('assets', ['clean:assets', 'copy:assets']);
+	grunt.registerTask('default', ['lint', 'assets']);
+
+};
