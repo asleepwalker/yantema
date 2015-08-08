c819851c1cc1cf307c9c11ee6b286b8baecf461c
Sleepwalker <mail@asleepwalker.ru>
2015-08-08 19:38:57 +0300

    Added login page view

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var posts = require('./routes/posts');
-var login = require('./routes/login');
 
 var app = express();
 
@@ -21,8 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/posts', posts);
-app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
 	res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res) {
+	res.render('login', { title: 'Express' });
+});
+
 module.exports = router;
